7eba366a509414649dcda13e23c18438f035f978
Jackson Chen <jacksontw92@gmail.com>
2022-08-04 00:50:20 +0900

    👍 my_calendar_1 binary tree

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ pub mod solution;
 mod binary_tree_inorder_traversal;
 mod count_odd_number_in_an_interval_range;
 mod kth_largest_element_in_an_array;
+mod kth_smallest_element_in_a_sorted_matrix;
+mod my_calendar_1;
 mod roman_to_int;
 mod unique_paths;
 mod valid_anagram;
 mod word_subsets;
-mod kth_smallest_element_in_a_sorted_matrix;
-mod my_calendar_1;
 
 fn main() {
 	println!("Hello, Leetcode!");
--- src/my_calendar_1.rs
+++ src/my_calendar_1.rs
@@ -1,24 +1,93 @@
+// struct MyCalendar {
+//     records: Vec<(i32,i32)>
+// }
+
+// 0729
+// Brute Force
+//impl MyCalendar {
+
+//     fn new() -> Self {
+//         Self {
+//             records: vec![]
+//         }
+//     }
+
+//     fn book(&mut self, start: i32, end: i32) -> bool {
+//         if self.records.iter().find(|(s,e)| *s < end && *e > start).is_some() {
+//             false
+//         } else {
+//             self.records.push((start, end));
+//             true
+//         }
+//     }
+// }
+
+// 0729
+// BinaryTree, but only 83.33% 33ms
+struct BinaryTree {
+	start: i32,
+	end: i32,
+	left: Option<Box<BinaryTree>>,
+	right: Option<Box<BinaryTree>>,
+}
+
+impl BinaryTree {
+	pub fn new(start: i32, end: i32) -> Self {
+		Self {
+			start,
+			end,
+			left: None,
+			right: None,
+		}
+	}
+
+	pub fn insert(&mut self, node: Box<BinaryTree>) -> bool {
+		if self.end > node.start && node.end > self.start {
+			false
+		} else {
+			if node.start < self.start {
+				if let Some(left) = &mut self.left {
+					left.insert(node)
+				} else {
+					self.left = Some(node);
+                    true
+				}
+			} else {
+				if let Some(right) = &mut self.right {
+					right.insert(node)
+				} else {
+					self.right = Some(node);
+                    true
+				}
+			}
+		}
+	}
+}
+
 struct MyCalendar {
-    records: Vec<(i32,i32)>
+	root: Option<Box<BinaryTree>>,
 }
 
 // 0729
 // Brute Force
 impl MyCalendar {
+	fn new() -> Self {
+		Self { root: None }
+	}
 
-    fn new() -> Self {
-        Self {
-            records: vec![]
-        }
-    }
-    
-    fn book(&mut self, start: i32, end: i32) -> bool {
-        if self.records.iter().find(|(s,e)| *s < end && *e > start).is_some() {
-            false
-        } else {
-            self.records.push((start, end));
-            true
-        }
-    }
+	fn book(&mut self, start: i32, end: i32) -> bool {
+		let curr = Box::new(BinaryTree {
+			start,
+			end,
+			left: None,
+			right: None,
+		});
+
+		if let Some(root) = &mut self.root {
+			root.insert(curr)
+		} else {
+			self.root = Some(curr);
+			true
+		}
+	}
 }
-
