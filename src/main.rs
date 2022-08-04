548ce5e91f6baf2600831be231615d2181bc9c2f
Jackson Chen <jacksontw92@gmail.com>
2022-08-04 13:29:06 +0900

    [feat] mirror_reflection

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod binary_tree_inorder_traversal;
 mod count_odd_number_in_an_interval_range;
 mod kth_largest_element_in_an_array;
 mod kth_smallest_element_in_a_sorted_matrix;
+mod mirror_reflection;
 mod my_calendar_1;
 mod roman_to_int;
 mod unique_paths;
---
+++ src/mirror_reflection.rs
@@ -0,0 +1,38 @@
+use crate::solution::Solution;
+
+// Time: O(log p)
+// Space: O(1)
+// 858
+impl Solution {
+	pub fn mirror_reflection(p: i32, q: i32) -> i32 {
+		let mut m = 1;
+		let mut n = 1;
+		while m * p != n * q {
+			n += 1;
+			m = n * q / p;
+		}
+
+		if m % 2 == 0 && n % 2 == 1 {
+			0
+		} else if m % 2 == 1 && n % 2 == 1 {
+			1
+		} else if m % 2 == 1 && n % 2 == 0 {
+			2
+		} else {
+			-1
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use test_case::test_case;
+
+	#[test_case(2, 1, 2)]
+	#[test_case(3, 1, 1)]
+	#[test_case(4, 3, 2)]
+	#[test_case(3, 2, 0)]
+	pub fn success_test(p: i32, q: i32, expect: i32) {
+		assert_eq!(super::Solution::mirror_reflection(p, q), expect);
+	}
+}
