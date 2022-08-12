948b5adfb81a72fb2abd7975f4a84919ff9aeb21
Jackson Chen <jacksontw92@gmail.com>
2022-08-12 23:34:23 +0900

    [feat] add testing of s0098

--- src/solutions/s0098_validate_binary_search_tree.rs
+++ src/solutions/s0098_validate_binary_search_tree.rs
@@ -1,4 +1,4 @@
-use crate::helpers::tree_node::TreeNode;
+use crate::helpers::tree::TreeNode;
 use crate::solutions::Solution;
 
 use std::cell::RefCell;
@@ -23,5 +23,27 @@ impl Solution {
 					&& Self::helper(root.right.clone(), val, max)
 			}
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use test_case::test_case;
+
+	use crate::helpers::tree::TreeNode;
+	use std::cell::RefCell;
+	use std::rc::Rc;
+
+	#[test_case(tree![5, 1, 4, null, null, 3, 6], false)]
+	#[test_case(tree![2, 1, 3], true)]
+	#[test_case(tree![10, 5, 15, null, null, 6, 20], false)]
+	#[test_case(tree![2147483647], true)]
+	#[test_case(tree![5,4,6,null,null,3,7], false)]
+	#[test_case(tree![0], true)]
+	#[test_case(tree![1,1,1], false)]
+	#[test_case(tree![1,null,1], false)]
+	#[test_case(tree![1,1,null], false)]
+	fn success_cases(node: Option<Rc<RefCell<TreeNode>>>, expected: bool) {
+		assert_eq!(super::Solution::is_valid_bst(node), expected);
 	}
 }
