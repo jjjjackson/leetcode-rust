b3d73bb34d44c21a68e73d3e9068aab9179c2a89
Jackson Chen <jacksontw92@gmail.com>
2022-09-08 11:25:11 +0900

    [feat] s0094

--- src/solutions/s0094_binary_tree_inorder_traversal.rs
+++ src/solutions/s0094_binary_tree_inorder_traversal.rs
@@ -1,43 +1,43 @@
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-	pub val: i32,
-	pub left: Option<Rc<RefCell<TreeNode>>>,
-	pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-	#[inline]
-	pub fn new(val: i32) -> Self {
-		TreeNode {
-			val,
-			left: None,
-			right: None,
-		}
-	}
-}
-
+use crate::helpers::tree::TreeNode;
 use crate::solutions::Solution;
+
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
-	pub fn inorder(
-		root: Option<Rc<RefCell<TreeNode>>>,
-		output: &mut Vec<i32>,
-	) -> Vec<i32> {
-		match root {
-			None => vec![],
-			Some(root) => {
-				Self::inorder(root.borrow().left.clone(), output);
-				output.push(root.borrow().val);
-				Self::inorder(root.borrow().right.clone(), output);
-				output.to_vec()
-			}
-		}
-	}
-
 	pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-		let mut result: Vec<i32> = vec![];
-		Self::inorder(root, &mut result)
+		root.map(|root| {
+			let mut root = root.borrow_mut();
+			[
+				Self::inorder_traversal(root.left.take()),
+				vec![root.val],
+				Self::inorder_traversal(root.right.take()),
+			]
+			.concat()
+		})
+		.unwrap_or_default()
 	}
 }
+
+// use std::cell::RefCell;
+// use std::rc::Rc;
+// impl Solution {
+// 	pub fn inorder(
+// 		root: Option<Rc<RefCell<TreeNode>>>,
+// 		output: &mut Vec<i32>,
+// 	) -> Vec<i32> {
+// 		match root {
+// 			None => vec![],
+// 			Some(root) => {
+// 				Self::inorder(root.borrow().left.clone(), output);
+// 				output.push(root.borrow().val);
+// 				Self::inorder(root.borrow().right.clone(), output);
+// 				output.to_vec()
+// 			}
+// 		}
+// 	}
+
+// 	pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+// 		let mut result: Vec<i32> = vec![];
+// 		Self::inorder(root, &mut result)
+// 	}
+// }
