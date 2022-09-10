a8454b5f0476b873b6523bacdc326482fb58a623
Jackson Chen <jacksontw92@gmail.com>
2022-09-10 23:41:35 +0900

    [refactor] fmt

--- src/solutions/s1996_the_number_of_weak_characters_in_the_game.rs
+++ src/solutions/s1996_the_number_of_weak_characters_in_the_game.rs
@@ -1,29 +1,28 @@
 use crate::solutions::Solution;
 
 impl Solution {
-    pub fn number_of_weak_characters(properties: Vec<Vec<i32>>) -> i32 {
-        use std::cmp::Ordering;
+	pub fn number_of_weak_characters(properties: Vec<Vec<i32>>) -> i32 {
+		use std::cmp::Ordering;
 
-        
-        let mut p = properties;
-        p.sort_by(|a,b| {
-            if ( a[0] == b[0] && a[1] > b[1] ) || a[0] < b[0] {
-                Ordering::Greater
-            } else {
-                Ordering::Less
-            }
-        });
-        
-        let mut max_till_now = i32::MIN;
-        let mut result = 0;
-        
-        for i in p {
-            if i[1] < max_till_now {
-                result += 1 ;
-            }
-            max_till_now = max_till_now.max(i[1]);
-        }
-        
-        result
-    }
+		let mut p = properties;
+		p.sort_by(|a, b| {
+			if (a[0] == b[0] && a[1] > b[1]) || a[0] < b[0] {
+				Ordering::Greater
+			} else {
+				Ordering::Less
+			}
+		});
+
+		let mut max_till_now = i32::MIN;
+		let mut result = 0;
+
+		for i in p {
+			if i[1] < max_till_now {
+				result += 1;
+			}
+			max_till_now = max_till_now.max(i[1]);
+		}
+
+		result
+	}
 }
